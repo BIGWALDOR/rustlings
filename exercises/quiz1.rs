9e426b5f40586c3cba29791ea07a5d679a46a298
Walid Bouladam <56755074+BIGWALDOR@users.noreply.github.com>
2024-06-08 16:26:58 +1200

    feat: complete first quiz.

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,10 +13,18 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
-
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples(apples: i32) -> i32 {
+    let mut price = 0;
+
+    if apples >40 {
+        price = apples * 1;
+        return price;
+    }
+
+    price = apples * 2;
+    return price;
+}
 
 // Don't modify this function!
 #[test]
